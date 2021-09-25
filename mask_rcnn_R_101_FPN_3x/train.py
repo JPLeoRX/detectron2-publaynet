8f0bc9cffc7fc53a0679ce86b9722c92011e698e
leo <leo.ertuna@gmail.com>
2021-09-26 01:38:24 +0300

    Masked R101-FPN achieved 86.690 box AP & 82.105 mask AP

---
+++ mask_rcnn_R_101_FPN_3x/train.py
@@ -0,0 +1,21 @@
+from detectron2.engine import DefaultTrainer
+from utils import build_config, register_publaynet_datasets
+
+# Register datasets
+dataset_train_name, dataset_test_name = register_publaynet_datasets()
+
+# Model parameters
+model_zoo_config_name = 'COCO-InstanceSegmentation/mask_rcnn_R_101_FPN_3x.yaml'
+trained_model_output_dir = '/home/leo/tekleo/detectron2-publaynet/mask_rcnn_R_101_FPN_3x/training_output'
+prediction_score_threshold = 0.7
+base_lr = 0.0001
+max_iter = 1000
+batch_size = 128
+
+# Detectron config
+cfg = build_config(model_zoo_config_name, dataset_train_name, dataset_test_name, trained_model_output_dir, prediction_score_threshold, base_lr, max_iter, batch_size)
+
+# Detectron Trainer
+trainer = DefaultTrainer(cfg)
+trainer.resume_or_load(resume=False)
+trainer.train()
